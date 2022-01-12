752fdc9e5afa427d4cc9313d5950c81ab90b8e41
Roger Díaz Viñolas <rdvdev2@gmail.com>
2022-01-12 18:57:20 +0100

    Don't overwrite output across compilers

--- src/compiler.cc
+++ src/compiler.cc
@@ -7,50 +7,61 @@ using namespace std;
 
 const string COMPILATION_TEXT = "Compilation";
 
-bool compile_file(const Compiler& compiler, const filesystem::path& source, const filesystem::path& output) {
+bool compile_file(const Compiler& compiler, const filesystem::path& source, const filesystem::path& output, const filesystem::path& errors) {
     DEBUG("Compiling " + source.string() + " to " + output.string());
     if (not filesystem::exists(source)) {
         DEBUG(source.string() + " doesn't exist!");
         return false;
     }
-    filesystem::path errors = output.parent_path() / "compilation.err";
     
     if (filesystem::exists(output)) {
         DEBUG("Removing previous compilation output: " + output.string());
         filesystem::remove(output);
     }
+
+    if (filesystem::exists(errors)) {
+        DEBUG("Removing previous compilation error output: " + errors.string());
+        filesystem::remove(errors);
+    }
+
     string command = compiler.command + " " + compiler.flags + " -c " + source.string() + " -o " + output.string() + " 2> " + errors.string();
     run_system_command(command);
 
     return filesystem::exists(output);
 }
 
-bool link_file(const Compiler& compiler, const filesystem::path& source, const filesystem::path& output) {
+bool link_file(const Compiler& compiler, const filesystem::path& source, const filesystem::path& output, const filesystem::path& errors) {
     DEBUG("Linking " + source.string() + " to " + output.string());
     if (not filesystem::exists(source)) {
         DEBUG(source.string() + " doesn't exist!");
         return false;
     }
-    filesystem::path errors = output.parent_path() / "compilation.err";
 
     if (filesystem::exists(output)) {
         DEBUG("Removing previous linking output: " + output.string());
         filesystem::remove(output);
     }
+
+    if (filesystem::exists(errors)) {
+        DEBUG("Removing previous linking error output: " + errors.string());
+        filesystem::remove(errors);
+    }
+
     string command = compiler.command + " " + compiler.flags + " " + source.string() + " -o " + output.string() + " 2> " + errors.string();;
     run_system_command(command);
 
     return filesystem::exists(output);
 }
 
-bool check_p1xx_compiles(const Problem& p) {
+bool check_p1xx_compiles(const Problem& p, filesystem::path& errors) {
     DEBUG("Checking if P1++ compiles the user's main.cc...");
     filesystem::path object_file = p.advocat_dir / "main.o";
+    errors = p.advocat_dir / "p1xx-compilation.err";
 
-    return compile_file(P1XX, p.source, object_file);
+    return compile_file(P1XX, p.source, object_file, errors);
 }
 
-bool compile_binary(const Problem& p, const filesystem::path& templates_dir) {
+bool compile_binary(const Problem& p, const filesystem::path& templates_dir, filesystem::path& errors) {
     DEBUG("Compiling the binary for testing with G++...");
     filesystem::path joined_source = p.advocat_dir / "joined.cc";
 
@@ -61,19 +72,25 @@ bool compile_binary(const Problem& p, const filesystem::path& templates_dir) {
 
     filesystem::path object_file = p.advocat_dir / "joined.o";
 
-    return compile_file(GXX, joined_source, object_file) and link_file(GXX, object_file, p.output);
+    errors = p.advocat_dir / "c1xx-compilation.err";
+    bool compiles = compile_file(GXX, joined_source, object_file, errors);
+    if (not compiles) return false;
+
+    errors = p.advocat_dir / "c1xx-linking.err";
+    return link_file(GXX, object_file, p.output, errors);
 }
 
 bool compile_problem(const Problem& p, const filesystem::path& templates_dir) {
     show_task_status(COMPILATION_TEXT, TaskType::Test, TaskStatus::InProgress);
 
-    bool compiles = check_p1xx_compiles(p) and compile_binary(p, templates_dir);
+    filesystem::path errors;
+    bool compiles = check_p1xx_compiles(p, errors) and compile_binary(p, templates_dir, errors);
 
     show_task_status(COMPILATION_TEXT, TaskType::Test, compiles ? TaskStatus::Pass : TaskStatus::Fail);
 
     if (not compiles) {
         string details;
-        read_file(p.advocat_dir / "compilation.err", details);
+        read_file(errors, details);
         show_details("Compilation output", details);
     }
 

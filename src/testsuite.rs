68179bb04c3c367180b714a11a6fc8f26c0efc37
Roger Díaz Viñolas <rdvdev2@gmail.com>
2022-02-08 16:06:22 +0100

    Refactor diff table building

--- src/testsuite.rs
+++ src/testsuite.rs
@@ -146,45 +146,94 @@ impl Test {
 // TODO: This is a mess, fix it
 fn parse_diff(diff: Vec<diff::Result<&str>>) -> (bool, String) {
     debug!("Parsing diff");
-    let side_width = ((ux::get_terminal_width() - 7) / 2) as usize;
     let mut pass = true;
-    let mut diff_text = format!("╭─{:—^side_width$}─┬─{:—^side_width$}─╮\n", "", "");
-    diff_text.push_str(format!("│ {:^side_width$} │ {:^side_width$} │\n", "Correct output", "Your output").as_str());
-    diff_text.push_str(format!("├─{:—^side_width$}─┼─{:—^side_width$}─┤\n", "", "").as_str());
+    let mut dd =
+        DiffDisplay::new("Expected output", "Your output", &color::Green, &color::Red);
 
     for line in diff {
         match line {
             diff::Result::Left(l) => {
                 pass = false;
-                diff_text.push_str(format!("│ {}{:side_width$}{} < {:side_width$} │\n",
-                                           color::Fg(color::Green),
-                                           cut_diff_line(l, side_width),
-                                           style::Reset, "").as_str());
+                dd.write_left(l);
             }
             diff::Result::Both(l, r) => {
-                diff_text.push_str(format!("│ {:side_width$} │ {:side_width$} │\n",
-                                           cut_diff_line(l, side_width),
-                                           cut_diff_line(r, side_width)).as_str());
+                dd.write_both(l, r);
             }
             diff::Result::Right(r) => {
                 pass = false;
-                diff_text.push_str(format!("│ {:side_width$} > {}{:side_width$}{} │\n",
-                                           "", color::Fg(color::Red),
-                                           cut_diff_line(r, side_width),
-                                           style::Reset).as_str());
+                dd.write_right(r);
             }
         }
     }
 
-    diff_text.push_str(format!("╰─{:—^side_width$}─┴─{:—^side_width$}─╯\n", "", "").as_str());
-    (pass, diff_text)
+    dd.end();
+    (pass, dd.text)
 }
 
-fn cut_diff_line(line: &str, max_width: usize) -> String {
-    if line.len() <= max_width {
-        line.to_owned()
-    } else {
-        let line = &line[0..max_width-3];
-        line.to_owned() + "..."
+
+
+struct DiffDisplay {
+    text: String,
+    side_width: usize,
+    left_color: &'static dyn color::Color,
+    right_color: &'static dyn color::Color
+}
+
+impl DiffDisplay {
+    fn new(left_title: &str, right_title: &str, left_color: &'static dyn color::Color, right_color: &'static dyn color::Color) -> Self {
+        let side_width = ((ux::get_terminal_width() - 7) / 2) as usize;
+        let mut dd = DiffDisplay { text: String::new(), side_width, left_color, right_color };
+        dd.draw_horizontal_line('╭', '┬', '╮');
+        dd.write_centered_row(left_title, right_title);
+        dd.draw_horizontal_line('├', '┼', '┤');
+        dd
+    }
+
+    fn draw_horizontal_line(&mut self, left: char, mid: char, right: char) {
+        self.text.push_str(format!("{l}─{:─^w$}─{m}─{:─^w$}─{r}\n", "", "",
+            l = left, m = mid, r = right, w = self.side_width).as_str());
+    }
+
+    fn end(&mut self) {
+        self.draw_horizontal_line('╰', '┴', '╯');
+    }
+
+    fn write_centered_row(&mut self, left: &str, right: &str) {
+        let left = self.trim_line(left);
+        let right = self.trim_line(right);
+        self.text.push_str(format!("│ {l:^w$} │ {r:^w$} │\n",
+            l = left, r = right, w = self.side_width).as_str());
+    }
+
+    fn write_row(&mut self, left: &str, mid: char, right: &str, left_color: &dyn color::Color, right_color: &dyn color::Color) {
+        let left = self.trim_line(left);
+        let right = self.trim_line(right);
+        self.text.push_str(format!("│ {lc}{l:w$}{nc} {m} {rc}{r:w$}{nc} │\n",
+            l = left, m = mid, r = right,
+            lc = color::Fg(left_color),
+            rc = color::Fg(right_color),
+            nc = color::Fg(color::Reset),
+            w = self.side_width).as_str());
+    }
+
+    fn write_left(&mut self, left: &str) {
+        self.write_row(left, '<', "", self.left_color, self.right_color);
+    }
+
+    fn write_right(&mut self, right: &str) {
+        self.write_row("", '>', right, self.left_color, self.right_color);
+    }
+
+    fn write_both(&mut self, left: &str, right: &str) {
+        self.write_row(left, '│', right, &color::Reset, &color::Reset);
+    }
+
+    fn trim_line(&self, line: &str) -> String {
+        if line.len() <= self.side_width {
+            line.to_owned()
+        } else {
+            let line = &line[0..self.side_width-3];
+            line.to_owned() + "..."
+        }
     }
 }

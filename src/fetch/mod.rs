63f656bfd26f7322b71e9c2fc1922998d5f2b030
Roger Díaz Viñolas <rdvdev2@gmail.com>
2022-02-20 18:39:44 +0100

    First steps towards the resource based system

--- src/fetch/mod.rs
+++ src/fetch/mod.rs
@@ -1,63 +1,43 @@
-use crate::{config, error, problem, ux, warning};
-use std::fmt;
+use crate::{compilation, config, error, problem, testing};
 
-mod connection_manager;
+pub mod connection_manager;
 mod credentials;
-mod download;
 mod unzip;
+pub mod resource;
 
 pub use credentials::Credentials;
+use crate::fetch::resource::Resource;
 
 pub fn fetch_resources(
     problem: &problem::Problem,
     config: &config::Config,
-) -> Result<(bool, bool, bool), crate::Error> {
-    let mut connection =
+) -> Result<(bool, bool), crate::Error> {
+    let connection =
         connection_manager::ConnectionManager::new(config).map_err(|e| crate::Error {
             description: format!("Couldn't start the connection manager: {}", e),
             exitcode: exitcode::IOERR,
         })?;
 
-    let zip = execute_task("Downloading problem zip", || {
-        download::download_problem_zip(problem, &mut connection)
-    });
-    let main_cc = execute_task("Downloading problem main.cc", || {
-        download::download_problem_main(problem, &mut connection)
-    });
-    let tests = execute_task("Extracting tests", || {
-        download::unzip_problem_tests(problem)
-    });
+    let mut compilation_resources: Vec<Box<dyn Resource>> = Vec::new();
+    compilation::P1XX.write_required_resources(problem, &connection, &mut compilation_resources);
+    let mut testing_resources: Vec<Box<dyn Resource>> = Vec::new();
+    testing::write_required_resources(problem, &connection, &mut testing_resources);
 
-    if !zip {
-        warning!("Unable to retrieve tests!");
+    let mut compilation = true;
+    for resource in compilation_resources {
+        if let Err(e) = resource.acquire() {
+            error!("Compilation resource acquisition failed: {}", e);
+            compilation = false;
+        }
     }
 
-    if !main_cc {
-        return Err(crate::Error {
-            description: String::from(
-                "Unable to retrieve the main.cc file, which is required to compile your binary!",
-            ),
-            exitcode: exitcode::IOERR,
-        });
+    let mut testing = true;
+    for resource in testing_resources {
+        if let Err(e) = resource.acquire() {
+            error!("Testing resource acquisition failed: {}", e);
+            testing = false;
+        }
     }
 
-    if !tests {
-        warning!("Unable to unzip tests!");
-    }
-
-    Ok((zip, main_cc, tests))
-}
-
-fn execute_task<T, E: fmt::Display + Sized>(name: &str, mut task: T) -> bool
-where
-    T: FnMut() -> (ux::TaskStatus, Option<E>),
-{
-    ux::show_task_status(name, ux::TaskType::Fetch, &ux::TaskStatus::InProgress);
-    let (status, err) = task();
-
-    ux::show_task_status(name, ux::TaskType::Fetch, &status);
-    if let Some(err) = err {
-        error!("The task [{}] returned the following error: {}", name, err);
-    }
-    status.is_ok()
+    Ok((compilation, testing))
 }
